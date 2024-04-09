d8fc30755693820e56c7bc51e4267d8019316462
ankitadubey2004 <155213532+ankitadubey2004@users.noreply.github.com>
2024-04-09 21:16:52 +0530

    js

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ next_btn.addEventListener('click',(e)=>{
     }
     changeSlide();
 })
+
+const scrollContainer = document.querySelectorAll('.products');
+for(const item of scrollContainer){
+    item.addEventListener('wheel',(evt)=>{
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+    });
+}
